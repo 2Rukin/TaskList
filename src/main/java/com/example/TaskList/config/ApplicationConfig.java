e7f6d7926f18ba74e0b2e0a979474f9e4111e7c5
2Rukin <97667192+2Rukin@users.noreply.github.com>
2023-08-12 14:24:17 +0300

    Создаем пакет Web.Security В этом пакете создает JwtEntity и имплементим спринговый UserDetails добавляем поля     private Long id;     private final String username;     private final String name;     private final String password;     private final Collection< ? extends GrantedAuthority> authorities;
        Здесь проверок не будет , поэтому рсставляем флажки на True

--- src/main/java/com/example/TaskList/config/ApplicationConfig.java
+++ src/main/java/com/example/TaskList/config/ApplicationConfig.java
@@ -1,9 +1,6 @@
 package com.example.TaskList.config;
 
-
-import com.example.TaskList.domain.user.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,42 +23,88 @@ public class ApplicationConfig {
 
     private final ApplicationContext applicationContext;
 
+    // Создание бина для шифрования паролей с использованием BCryptPasswordEncoder
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // Создание бина для управления аутентификацией
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
 
+    // Создание SecurityFilterChain для настройки безопасности HTTP
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                // Отключение защиты CSRF
                 .csrf().disable()
+                // Настройка CORS  (Cross-Origin Resource Sharing) для безопасности при работе с разными источниками данных
                 .cors()
                 .and()
+                // Отключение HTTP Basic аутентификации
                 .httpBasic().disable()
+                // Настройка управления сессиями
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                // Обработка исключений
                 .exceptionHandling()
+                // Установка обработчика начальной аутентификации
                 .authenticationEntryPoint((request, response, authException) -> {
                     response.setStatus(HttpStatus.UNAUTHORIZED.value());
                     response.getWriter().write("Unauthorized.");
                 })
+                // Установка обработчика доступа запрещен
                 .accessDeniedHandler((request, response, accessDeniedException) -> {
                     response.setStatus(HttpStatus.FORBIDDEN.value());
                     response.getWriter().write("Unauthorized.");
                 })
                 .and()
+                // Настройка правил авторизации запросов
                 .authorizeHttpRequests()
+                // Разрешение доступа к определенному пути без аутентификации
                 .requestMatchers("/api/v1/auth/**").permitAll()
+                // Остальные запросы требуют аутентификации
                 .anyRequest().authenticated()
                 .and()
+                // Отключение анонимной аутентификации
                 .anonymous().disable()
+                // Добавление JwtTokenFilter перед фильтром аутентификации
                 .addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
+
+    //альтернативная от чата
+/*
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exception -> {
+                    exception.authenticationEntryPoint((request, response, authException) -> {
+                        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                        response.getWriter().write("Unauthorized.");
+                    });
+                    exception.accessDeniedHandler((request, response, accessDeniedException) -> {
+                        response.setStatus(HttpStatus.FORBIDDEN.value());
+                        response.getWriter().write("Unauthorized.");
+                    });
+                })
+                .authorizeRequests(requests -> {
+                    requests
+                            .antMatchers("/api/v1/auth/**").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .anonymous(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JwtTokenFilter(jwtTokenProvider), JwtTokenFilter.class);
+        return httpSecurity.build();
+    }
+
+ */
 }
