873c74f720fa81123d6f59cd472cea047d023075
2Rukin <97667192+2Rukin@users.noreply.github.com>
2023-08-23 11:00:38 +0300

    Требования для класса, чтобы он стал сущностью (entity) для Hibernate Аннотация @Entity аннотацией @Id            @GeneratedValue(strategy = GenerationType.UUID)                                       SEQUENCE IDENTITY                                        TABLE   AUTO - Конструктор без Параметров   - мог создавать экземпляры при загрузке данных из базы - Геттеры и Сеттеры - Дефолтное значение hashCode() и equals() @EqualsAndHashCode добавляет реализацию - Отсутствие final: иметь возможность создавать прокси
    также можно указывать
    @Column - будет создавать соответствующую колонку

--- src/main/java/com/example/tasklist/domain/user/User.java
+++ src/main/java/com/example/tasklist/domain/user/User.java
@@ -1,15 +1,25 @@
 package com.example.tasklist.domain.user;
 
 import com.example.tasklist.domain.task.Task;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
 import java.util.Set;
 
 @Data
+@Entity
+@NoArgsConstructor
+@EqualsAndHashCode
 public class User implements Serializable {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
     private String name;
     private String username;
